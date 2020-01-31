5f27779104e64062dde3d4ea4dd30501a9b48f30
MilanMlinar <mm223kg@student.lnu.se>
2020-01-31 03:54:47 +0100

    test commit

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -13,8 +13,8 @@ const forecast = (latitude, longitude, cb) => {
             cb(body.error, undefined)
         } else {
             cb(undefined, 
-            `${body.daily.data[0].summary}\nIt is currently ${body.currently.temperature} degrees out.
-            The high today is ${body.daily.data[0].temperatureHigh} degrees with a low of ${body.daily.data[0].temperatureLow} degrees.\n
+            `${body.daily.data[0].summary} It is currently ${body.currently.temperature} degrees out.
+            The high today is ${body.daily.data[0].temperatureHigh} degrees with a low of ${body.daily.data[0].temperatureLow} degrees. 
             There is a ${body.currently.precipProbability}% chance of rain.`)
         }
     })
